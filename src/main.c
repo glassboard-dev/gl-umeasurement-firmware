c681e0e271c8741c7aa689a72bc3aa9edad39ec1
stephendpmurphy <stephenmurphy@glassboard.com>
2022-01-14 08:57:24 -0500

    ADC samples reported over USB

--- src/main.c
+++ src/main.c
@@ -30,39 +30,14 @@ SOFTWARE.
 #include "adc.h"
 #include "gpio.h"
 #include "ui.h"
-
-#define ADC_READ_RATE_uS    1
-#define ADC_REPORT_RATE_uS  10
-#define ADC_BUFF_SIZE       30000
+#include "hid_generic.h"
 
 #define ADC_COUNT_OFFSET    13636
 
-uint32_t adc_read_refTime = 0x0000;
-uint32_t adc_report_refTime = 0x0000;
-uint16_t adc_count = 0;
-uint16_t adc_samples[ADC_BUFF_SIZE] = {0};
-uint16_t max_reported = 0;
-bool adc_reporting = false;
-char adc_report[5120] = {0};
-char tmpLine[32] = {0};
+uint8_t rx_buffer[1024] = {0x00};
+uint8_t tx_buffer[1024] = {0x00};
 
 void adc_cb(uint16_t count);
-
-static void reportADC_samples(void) {
-    adc_reporting = true;
-
-    if( adc_count > max_reported)
-        max_reported = adc_count;
-
-    memset(adc_report, 0x00, sizeof(adc_report));
-    // sprintf(adc_report, "%d samples collected in %dms - Max: %d\n\r", adc_count, ADC_REPORT_RATE_uS/1000, max_reported);
-    sprintf(adc_report, "ADC: %d\n\r", (adc_samples[0]) - ADC_COUNT_OFFSET);
-    PRINTF("%s", adc_report);
-
-    adc_count = 0;
-
-    adc_reporting = false;
-}
 
 int main(void)
 {
@@ -70,39 +45,22 @@ int main(void)
     BOARD_Init();
 
     // Module init
+    USB_DeviceApplicationInit(tx_buffer, rx_buffer);
     adc_init(adc_cb);
     ui_init();
 
+    // Other init code
     PRINTF("Glasslabs - uMeasurement\n\r");
 
     while (1)
     {
         // Run our UI update function
         ui_update();
-
-        // Start an async read of the ADC
-        if( (adc_read_refTime + ADC_READ_RATE_uS) < BOARD_GetTick() ) {
-            // if( !adc_reporting )
-                adc_read();
-            adc_read_refTime = BOARD_GetTick();
-        }
-
-        // Start an async read of the ADC
-        if( (adc_report_refTime + ADC_REPORT_RATE_uS) < BOARD_GetTick() ) {
-            reportADC_samples();
-            adc_report_refTime = BOARD_GetTick();
-        }
+        adc_read();
     }
 }
 
 void adc_cb(uint16_t count) {
-    // if( adc_reporting )
-    //     return;
-
-    if( adc_count >= ADC_BUFF_SIZE)
-        return;
-
-    adc_samples[adc_count] = count;
-    adc_count++;
-    // PRINTF("New adc measurement!: %d\n\r", count);
+    tx_buffer[0] = (uint8_t)(count);
+    tx_buffer[1] = (uint8_t)(count >> 8);
 }
